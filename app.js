a270de0cf55d7a4f93560123544e708b13f6f9b3
Michael <agne.michele@gmail.com>
2018-12-15 20:14:33 +0100

    Add get api and test case

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const getArea = require('./area');
 
 const app = express();
 
@@ -7,10 +8,18 @@ app.use(bodyParser.json());
 
 app.set('port', (process.env.PORT || 3000));
 
-console.log(app.prototype);
-
 app.get('/', (req, res) => {
    res.send("Hello");
+});
+
+app.get('/getArea', (req, res) => {
+   const base = Number.parseInt(req.query.side1);
+   const altezza = Number.parseInt(req.query.side2);
+
+   if(Number.isInteger(base) && Number.isInteger(altezza))
+      res.status(200).send(JSON.stringify({area: getArea([base, altezza])}));
+   else
+      res.status(400).end();
 });
 
 app.listen(app.get('port'), () => {
--- area.test.js
+++ area.test.js
@@ -1,4 +1,18 @@
+const fetch = require('node-fetch');
 const getArea = require('./area');
+
+const PORT = process.env.PORT || 3000;
+const SERVER_URL = process.env.SERVER_URL || 'http://localhost:' + PORT;
+
+const callGet = function(b, h){
+   return fetch(SERVER_URL + '/getArea?side1=' + b + '&side2=' + h, {
+      method: 'GET',
+      headers: {
+         'Content-Type': 'application/json',
+         'Accept': 'application/json'
+      }
+   });
+}
 
 //Test integers, zero, null, negative, positive, strings
 test('Test for non integers', () => {
@@ -19,4 +33,16 @@ test('Test for negative numbers', () => {
 
 test('Test for positive numbers', () => {
    expect(getArea([2, 3])).toBe(6);
+});
+
+test('Test del corretto funzionamento del servizio', () => {
+   return callGet(3, 4)
+      .then(res => {
+         expect(res.status).toBe(200);
+         return res.json();
+      })
+      .then(jres => {
+         expect(jres).toEqual({area: 12});
+      })
+      .catch(e => console.log(e));
 });
